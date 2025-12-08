bd409a555a647586ee8612920424a1563492b23e
Nitin Singh <45931833+nitsword@users.noreply.github.com>
2025-12-08 16:26:11 +0530

    Update outputs.tf

--- aws-final-v5-vpce-dep-imp/modules/nacls/outputs.tf
+++ aws-final-v5-vpce-dep-imp/modules/nacls/outputs.tf
@@ -16,34 +16,33 @@ output "private_nacl_id" {
 
 # --- NACL Association IDs ---
 
-# Collects all public subnet associations, keyed by availability zone letter (a, b, c).
 
 output "public_associations" {
-  description = "A map of public subnet association IDs, keyed by AZ letter."
+  description = "public subnet association ID"
   value = {
     for k, v in aws_network_acl_association.public_assoc : k => v.id
   }
 }
 
-# Collects all private subnet associations, keyed by availability zone letter (a, b, c).
+#private subnet associations
 output "private_tier_associations" {
-  description = "A map of private subnet association IDs, keyed by AZ letter."
+  description = "private subnet association IDs"
   value = {
     for k, v in aws_network_acl_association.private_assoc : k => v.id
   }
 }
 
-# Collects all nonroutable subnet associations, keyed by availability zone letter (a, b, c).
+
 output "nonroutable_tier_associations" {
-  description = "A map of nonroutable subnet association IDs, keyed by AZ letter."
+  description = "nonroutable subnet association ID"
   value = {
     for k, v in aws_network_acl_association.nonroutable_assoc : k => v.id
   }
 }
 
-# Combines all non-public associations (Private and Non-Routable) into a single map.
+
 output "all_non_public_associations" {
-  description = "Combined map of all Private and Non-Routable subnet association IDs, keyed by AZ letter."
+  description = "Private and Non-Routable subnet association ID"
   value = merge(
     { for k, v in aws_network_acl_association.private_assoc : k => v.id },
     { for k, v in aws_network_acl_association.nonroutable_assoc : k => v.id }
