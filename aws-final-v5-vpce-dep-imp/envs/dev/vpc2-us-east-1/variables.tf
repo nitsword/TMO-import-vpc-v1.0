30e8c6de7a2dc559966583854000c1106b8d79bc
Nitin Singh <45931833+nitsword@users.noreply.github.com>
2025-12-08 16:44:39 +0530

    Update variables.tf

--- aws-final-v5-vpce-dep-imp/envs/dev/vpc2-us-east-1/variables.tf
+++ aws-final-v5-vpce-dep-imp/envs/dev/vpc2-us-east-1/variables.tf
@@ -22,7 +22,6 @@ variable "vpc" {
 
 #################################################
 # SUBNETS (public / private / nonroutable)
-# Each must be a map with stable keys (a, b, c)
 #################################################
 
 variable "subnets" {
@@ -189,9 +188,9 @@ variable "tags" {
 }
 
 variable "availability_zone_keys" {
-  description = "A static, known set of keys (e.g., 'a', 'b', 'c') corresponding to the subnets to be deployed. This is critical for for_each iteration."
+  description = "static keys"
   type        = set(string)
   # --- CHANGE IS HERE ---
-  default     = ["a", "b", "c"] # Now deploying to three AZs
+  default     = ["a", "b", "c"]
   # ----------------------
 }
